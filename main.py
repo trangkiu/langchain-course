fc1f38bb8247d0ee85224fa07f4140f2b466fa82
trangkiu <kiuarchives@gmail.com>
2026-03-23 23:14:24 -0400

    add formating with pydantic

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+from pydantic import BaseModel, Field
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -25,12 +28,24 @@ from langchain_tavily import TavilySearch
 #  tools = [search]
 
 
+# this is to format the response
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url:str = Field(description="Source URL")
+
+class AgentResponse(BaseModel):
+    """Schema for a response from the agent"""
+    answer:str = Field(description="Response from the agent")
+    source:List[Source] = Field(default_factory=List, description="Source used by the agent")
+
+
+
 llm = ChatOpenAI(model="gpt-5")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
-    result = agent.invoke({"messages": HumanMessage(content="What is the weather in London")})
+    result = agent.invoke({"messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?")})
     print(result)
 
 if __name__ == "__main__":
